8aec7d5829907ff4203532d9b1fb7f104195b679
Grace C. Felho <gfelho@purdue.edu>
2025-04-03 19:21:59 -0400

    passing first two; init

--- linked_list.c
+++ linked_list.c
@@ -2,17 +2,61 @@
 
 #include <stdlib.h>
 
-struct list_node *new_node(size_t value) { return NULL; }
+struct list_node *new_node(size_t value) { 
+  // write this too
+  struct list_node * n = malloc(sizeof(struct list_node));
+  n->value = value;
+  n->next = NULL;
+  return n;
+}
 
-void insert_at_head(struct linked_list *list, size_t value) {}
+void insert_at_head(struct linked_list *list, size_t value) {
+  struct list_node * p = new_node(value);
+  p->next = list->head;
+  // p = list->head;
+  list->head = p;
+} //checked this function
 
-void insert_at_tail(struct linked_list *list, size_t value) {}
+void insert_at_tail(struct linked_list *list, size_t value) {
+  struct list_node * p = new_node(value);// check slide 118 of 20250304linkedlist
+  struct list_node * q = list->head;
+  if(q==NULL){return;} // probably not necessary
+  while(p->next!=NULL){
+    p=p->next;
+  }
+  p->next = q;
+}
 
-size_t remove_from_head(struct linked_list *list) { return 0; }
+size_t remove_from_head(struct linked_list *list) { 
+  struct list_node * p = list->head;
+  size_t value = p->value;
+  list->head = p->next;
+  free(p); 
+  return value;
+}
 
-size_t remove_from_tail(struct linked_list *list) { return 0; }
+size_t remove_from_tail(struct linked_list *list) {
+  struct list_node * p = list->head;
+  if(p->next==NULL){
+    list->head= NULL;
+    return p->value;
+  }
+  while (p->next->next != NULL){
+    p->next = p->next->next;
+  }
+  size_t value = p->next->value;
+  free(p->next);
+  return value;
+}
 
-void free_list(struct linked_list list) {}
+void free_list(struct linked_list list) { //had to add a * to list???
+  struct list_node * p = list.head;
+  while (list.head != NULL){
+    list.head = p->next;
+    free(p);
+    p->next = p->next->next;
+  }
+}
 
 // Utility function to help you debugging, do not modify
 void dump_list(FILE *fp, struct linked_list list) {
--- queue.c
+++ queue.c
@@ -1,8 +1,64 @@
 #include "queue.h"
 #include "tile_game.h"
+#include <stdlib.h>
 
-void enqueue(struct queue *q, struct game_state state) {}
+void enqueue(struct queue *q, struct game_state state) {
+    //encode
+    uint64_t encoded; //stored serialized game state
+    //serialize
+    encoded = serialize(state);
+    //insert
+    insert_at_head(&q->data, encoded); //void function
+}
 
-struct game_state dequeue(struct queue *q) { return (struct game_state){0}; }
+struct game_state dequeue(struct queue *q) {
+    struct game_state state;
+    //deserialize
+    state = deserialize(remove_from_tail(&(q->data))); 
+    return state;
+}
 
-int number_of_moves(struct game_state start) { return 0; }
+int number_of_moves(struct game_state start) { 
+
+
+    struct queue q = { .data.head = NULL }; // = malloc(sizeof(struct queue));
+    //enqueue
+    struct game_state curr = start;
+    enqueue(&q, start);
+    // I don't know where to put all of these
+    // these need to go in the queue but I'm not sure how
+    while (q.data.head!=NULL){
+        curr = dequeue(&q);
+        if(start.empty_row > 0){
+            move_up(&curr);
+            enqueue(&q,curr);
+        }
+        if(curr.empty_row < 3){
+            move_down(&curr);
+            enqueue(&q,curr);
+        }
+        if(curr.empty_col > 0){
+            move_left(&curr);
+            enqueue(&q,curr);
+        }
+        if(curr.empty_col < 3){      
+            move_right(&curr);
+            enqueue(&q,curr);
+        }
+        //successful?
+        if(curr.empty_row == 3 && curr.empty_col == 3){
+            int correct = 1;
+            for(int i = 0; i < 4; i++){
+                for(int j = 0; j < 4; j++){
+                    if(curr.tiles[i][j]==correct){
+                        if(correct==15){
+                            return curr.num_steps;
+                        }
+                        correct++;
+                    }
+                }
+            }
+        }
+    }
+    return 0; 
+}
